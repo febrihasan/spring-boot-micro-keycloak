6f5b3c75adb9bde1aafc68f23c473b3a7fc41a99
Febri Hasan <febri.hasan@akarinti.tech>
2023-01-05 09:08:09 +0700

    feat: add payment service

---
+++ payment-service/src/test/java/io/febrihasan/transaction/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.febrihasan.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
